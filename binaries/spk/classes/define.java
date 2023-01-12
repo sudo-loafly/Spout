995f13e8c9ea64b9b9f32f1186e42d8d805a724a
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2023-01-12 14:53:49 -0500

    Defines the environmental variables in Spout.
    Also a keyword in "Spout"

---
+++ binaries/spk/classes/define.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+// Stale, full-package importation...
+package org.staledeveloperkit.binaries;
+package org.staledeveloperkit.binaries.runtime;
+package org.staledeveloperkit.binaries.utilites;
+package org.staledeveloperkit.binaries.os;
+package org.staledeveloperkit.binaries.sources;
+
+// TokeScript, Java importation...
+package com.tksdevkit.binaries.*;
+public class define extends SPK {
+  private static method(String input) {
+    Scanner ipt = new Scanner(System.in);
+    String input = ipt.nextLine();
+    if (current(method)) {
+      send_full_status_report();
+      send_full_utility_networker();
+    } else {
+      this.call('func') -> 'void'
+        || 'network';
+      return Stale as Object;
+    }
+  }
+}
