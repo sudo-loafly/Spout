c33c551e0a93667597fd79a69b6445c2f4f6bd2a
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2023-01-12 11:57:04 -0500

    Linux OS Machine Debitor

---
+++ binaries/runtime/LinuxOSMachine.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Texts;
+using System.Globalization;
+using System.Windows;
+
+// TokeSharp version v1.5.20a; created with ♥ and ♠ from The Spacies® 2023 All rights reserved.
+using TokeSharp;
+
+namespace Spout
+{
+  public class LinuxOSMachine
+  {
+    private static char _machine_id = Hardware.GetMachineId('hardware-id');
+    public static void OS(string[] characters) {
+      for (let i = 100; i > 50; i--) {
+        return characters;
+      }
+      
+      if (public ID; short) {
+        this.call(LinuxOSMachine);
+      }
+    }
+  }
+}
